6a35a070759d2459056362d74eec3f9b9cec74bb
Najam Rain <najamrain@gmail.com>
2020-05-01 21:25:03 +0400

    Tech2bit Project

---
+++ java/aboutA.js
@@ -0,0 +1,128 @@
+window.sr = ScrollReveal();
+sr.reveal(".nst-heading1", {
+  duration: 2000,
+  origin: "left",
+  distance: "500px",
+});
+sr.reveal(".ttext1", {
+  duration: 2000,
+  origin: "top",
+  distance: "500px",
+});
+
+sr.reveal(".ttext2", {
+  duration: 2000,
+  origin: "right",
+  distance: "500px",
+});
+sr.reveal(".ancher-baba", {
+  duration: 2000,
+  origin: "left",
+  distance: "500px",
+});
+
+sr.reveal(".sc1-box1", {
+  duration: 2000,
+  origin: "left",
+  distance: "200px",
+});
+sr.reveal(".sc1-box2", {
+  duration: 2000,
+  origin: "top",
+  distance: "200px",
+});
+
+sr.reveal(".sc221-box3", {
+  duration: 2000,
+  origin: "right",
+  distance: "200px",
+});
+
+sr.reveal(".ddd1", {
+  duration: 2000,
+  origin: "left",
+  distance: "500px",
+});
+
+sr.reveal("#testimonial div", {
+  duration: 2000,
+  origin: "bottom",
+});
+sr.reveal(".info-left", {
+  duration: 2000,
+  origin: "left",
+  distance: "300px",
+  viewFactor: 0.2,
+});
+sr.reveal(".info-right", {
+  duration: 2000,
+  origin: "right",
+  distance: "300px",
+  viewFactor: 0.2,
+});
+
+sr.reveal(".nested-sc5-boxes", {
+  duration: 2000,
+  origin: "left",
+  distance: "500px",
+});
+
+sr.reveal("..showcase7", {
+  duration: 2000,
+  origin: "bottom",
+  distance: "100px",
+});
+
+sr.reveal(".sc5-box2", {
+  duration: 2000,
+  origin: "right",
+  distance: "100px",
+});
+
+sr.reveal(".sc2-box1", {
+  duration: 2000,
+  origin: "left",
+  distance: "300px",
+});
+
+sr.reveal(".sc2-box2", {
+  duration: 2000,
+  origin: "right",
+  distance: "300px",
+});
+
+sr.reveal(".sc3-box1", {
+  duration: 2000,
+  origin: "left",
+  distance: "300px",
+});
+
+sr.reveal(".sc3-box2", {
+  duration: 2000,
+  origin: "right",
+  distance: "300px",
+});
+
+sr.reveal(".managed-it", {
+  duration: 3000,
+  origin: "top",
+  distance: "50px",
+});
+
+sr.reveal(".border", {
+  duration: 2000,
+  origin: "left",
+  distance: "200px",
+});
+
+sr.reveal(".border1", {
+  duration: 2000,
+  origin: "left",
+  distance: "200px",
+});
+
+sr.reveal("data-target", {
+  duration: 2000,
+  origin: "left",
+  distance: "200px",
+});
---
+++ java/navbar.js
@@ -0,0 +1,10 @@
+window.addEventListener("scroll", function ()
+{
+    document.getElementById("naaj").style.backgroundColor = " #2f4b78";
+
+    document.getElementById("naaj").style.position = "fixed";
+    document.getElementById("naaj").style.top = "0";
+    document.getElementsByClassName("navbar-nav li a").style.color = "red";
+
+});
+
